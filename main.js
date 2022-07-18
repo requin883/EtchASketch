5f4e719e188984e4f020794f073d13125eb13908
requin883 <requin883@gmail.com>
2022-07-17 20:34:13 -0400

    feat: change button functionality, add input and functionality to input

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-let starter;
+let starter = 16;
 const startBtn = document.querySelector(".btn-table");
-const resetBtn = document.querySelector(".reset");
 const container = document.querySelector(".container");
-const newbtns = document.querySelectorAll(".table");
+const tableSize = document.querySelector(".table-size");
+tableSize.style.display = "none";
+container.style.display = "grid";
 startBtn.addEventListener('click',createTable);
 
 function createTable(){
-    if (starter==undefined){
-    container.style.display = "grid";
-    container.style.gridTemplateColumns= `repeat(16,1fr)`;
-    container.style.gridTemplateRows = "repeat(16,1fr)";
+    let newValue = Number(tableSize.value) || starter;
+    console.log(newValue);
+    container.innerHTML = "";
+    container.style.gridTemplate= `repeat(${newValue},max-content) / repeat(${newValue},max-content)`;
     container.style.gap = 0;
+    for (let i=0;i<newValue;i++){
+        for(let j=0;j<newValue;j++){
+            let newNode = document.createElement("div");
+            newNode.classList.add("table");
+            container.appendChild(newNode);
+            newNode.id = `${i}-${j}`;
+        }
+        }
+    const newbtns = document.querySelectorAll(".table");
+    if(tableSize.style.display == "none"){
+        tableSize.style.display = "inline-block";
     }
-for (let i=0;i<16;i++){
-    for(let j=0;j<16;j++){
-        let newNode = document.createElement("div");
-        newNode.classList.add("table");
-        container.appendChild(newNode);
-        newNode.id = `${i}-${j}`;
-    }
-    }
+    tableSize.value = "";
+    newbtns.forEach(button=>button.addEventListener('mouseover',(e)=>{
+        e.target.style.backgroundColor="red";
+    }));
+
 }
 
-newbtns.forEach(button=>button.addEventListener('mouseover',(e)=>{
-    e.target.style.background="lightblue";
-}));
+
+
+
 
 
