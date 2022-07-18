74fcc75fe83ab10f84c732a58718894f240f6110
requin883 <requin883@gmail.com>
2022-07-17 20:44:51 -0400

    hotfix: remove input and change functionality for prompt user

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let starter = 16;
 const startBtn = document.querySelector(".btn-table");
 const container = document.querySelector(".container");
-const tableSize = document.querySelector(".table-size");
-tableSize.style.display = "none";
 container.style.display = "grid";
 startBtn.addEventListener('click',createTable);
 
 function createTable(){
-    let newValue = Number(tableSize.value) || starter;
-    console.log(newValue);
+    let newValue = prompt("Input a number between 1-100");
+    while(!Number(newValue) || (Number(newValue)>100 && Number(newValue)>1)){
+        newValue = prompt("Please input a number between 1-100");
+    }
     container.innerHTML = "";
     container.style.gridTemplate= `repeat(${newValue},max-content) / repeat(${newValue},max-content)`;
     container.style.gap = 0;
@@ -21,10 +21,6 @@ function createTable(){
         }
         }
     const newbtns = document.querySelectorAll(".table");
-    if(tableSize.style.display == "none"){
-        tableSize.style.display = "inline-block";
-    }
-    tableSize.value = "";
     newbtns.forEach(button=>button.addEventListener('mouseover',(e)=>{
         e.target.style.backgroundColor="red";
     }));
